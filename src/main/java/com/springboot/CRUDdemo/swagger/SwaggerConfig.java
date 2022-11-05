42827901e95206ea5e531849a65cf489a5b5eee8
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-11-05 16:09:38 -0500

    implmented security and swaggerui

---
+++ src/main/java/com/springboot/CRUDdemo/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.springboot.CRUDdemo.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket getDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("public-apis")
+				.apiInfo(getApiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.build();
+	}
+
+	public ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("Tavarali code")
+				.description("API created by Tavarali code").version("1").build();
+	}
+
+}
